4626ece6249164748446859e313d515edf4de40e
Shokhrukh_Developer1 <shokhrukhdeveloper1@gmail.com>
2023-01-17 15:53:00 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+System.Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+string Kescha = $"Hello! {name} \nMy name is Kescha";
+System.Console.WriteLine(Kescha);
+System.Console.Write("Enter your age: ");
+int age =Convert.ToInt32(Console.ReadLine());
+int KeschaAge,difference;
+System.Console.Write("Kescha's age: ");
+KeschaAge=Convert.ToInt32(Console.ReadLine());
+if(age>KeschaAge)
+{
+  difference=age-KeschaAge;
+  System.Console.WriteLine($"{name} aka yoshimiz orasidagi farq {difference} ga teng. \nBrat siz mendan katta ekansiz!!!");
+}
+else if(age<KeschaAge)
+{
+  difference=KeschaAge-age;
+  System.Console.WriteLine($"{name} yoshimiz orasidagi farq {difference} ga teng. \nBratishka kichkina ekansanku!!!");
+}
+else
+{
+  System.Console.WriteLine($"{name} jo'ra, ikkalamiz teng ekanmiz!");
+}
